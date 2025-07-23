1892aebec8e522416c1a41b08278f1874bc5f3c2
Nellybii <nellychepkem2015@gmail.com>
2025-07-23 14:21:49 +0300

    add logo  to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'FurniStore',
   description: 'Furniture eCommerce app',
+  icons: {
+    icon: '/Logo.png',
+  },
 };
 
 export default function RootLayout({
